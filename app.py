12b57051e4547abeae2e12c939573524cab1b441
Nimesh <50800193+Nimesh04@users.noreply.github.com>
2024-08-08 12:56:28 -0500

    Changed the database structure, added dynamic routing feature for all the Mangas that are added to be opened to their specific page and show the list of the latest chapter

--- app.py
+++ app.py
@@ -5,24 +5,23 @@ import requests
 
 app = Flask(__name__)
 
-
 # creates the database table to store the updates on the chapter of manga
 def init_db():
-    with sqlite3.connect('manga_updates.db') as conn:
+    with sqlite3.connect('manga_list.db') as conn:
         cursor = conn.cursor()
+        cursor.execute('PRAGMA foreign_keys = ON;')
+       # Create manga_updates table
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS manga_updates(
                 id INTEGER PRIMARY KEY,
                 chapter TEXT,
-                date TEXT
+                date TEXT,
+                manga_id INTEGER,
+                FOREIGN KEY (manga_id) REFERENCES manga_list(id)
             )
         ''')
-        conn.commit()
-
-#creates the database table to store the names of manga
-def manga_list_db():
-    with sqlite3.connect('manga_list.db') as conn:
-        cursor = conn.cursor()
+        
+         # Create manga_list table
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS manga_list(
                     id INTEGER PRIMARY KEY,
@@ -36,7 +35,7 @@ def manga_list_db():
 #routes to home page of the website and displays the data stored in the manga-list
 @app.route('/')
 def home():
-    manga_list_db()
+    init_db()
     with sqlite3.connect('manga_list.db') as conn:
         cursor = conn.cursor()
         cursor.execute('SELECT manga_name, image_link FROM manga_list')
@@ -49,31 +48,31 @@ def add_manga():
     if request.method == 'POST':
         manga_name = request.form['manga_name']
         manga_link = request.form['manga_link']
-        with sqlite3.connect('manga_list.db') as conn:
-            cursor = conn.cursor()
-            cursor.execute('''
-                INSERT INTO manga_list (manga_name, url_link)
-                VALUES (?, ?)
-            ''', (manga_name, manga_link))
-            conn.commit()
+        add_manga_to_db(manga_name, manga_link)
+        scrape_manga(manga_name, manga_link)
         return redirect('/')
     return render_template("add-manga.html")
 
 #routes to manga page and displays the data of the chapters in the manga-list
-@app.route('/nano-machine')
-def nano():
+def nano(manga_name):
     init_db()
-    with sqlite3.connect('manga_updates.db') as conn:
+    with sqlite3.connect('manga_list.db') as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT chapter, date FROM manga_updates')
-        manga_updates = cursor.fetchall()
+        cursor.execute('SELECT id FROM manga_list WHERE manga_name = ?', (manga_name,))
+        result = cursor.fetchone()
+        if result:
+            manga_id = result[0]
+            cursor.execute('SELECT chapter, date FROM manga_updates WHERE manga_id = ?', (manga_id,))
+            manga_updates = cursor.fetchall()
+        else:
+            manga_updates = []
 
-    return(render_template("nano-machine.html", manga_updates = manga_updates))
+    return(render_template("manga_detail.html", manga_updates = manga_updates, manga_name=manga_name))
 
 #scrapes the chapter's name and data from a given url and stores them in the database
-@app.route('/scrape', methods=['POST'])
-def scrape_manga():
-    r = requests.get("https://kingofshojo.com/manga/nano-machine/")
+def scrape_manga(manga_name, manga_link_scrape):
+    init_db()
+    r = requests.get(manga_link_scrape)
 
     #scrape the html and find the div that contains the chapter lists
     soup = BeautifulSoup(r.content, 'html.parser')
@@ -88,16 +87,18 @@ def scrape_manga():
         cleaned_data = [(cleaned_text[i], cleaned_text[i + 1]) for i in range(0, len(cleaned_text), 2)]
 
         # Update the database
-        with sqlite3.connect('manga_updates.db') as conn:
+        with sqlite3.connect('manga_list.db') as conn:
             cursor = conn.cursor()
-            for chapter, date in cleaned_data:
-                cursor.execute('''
-                    INSERT INTO manga_updates(chapter, date)
-                    VALUES (?, ?)
-                ''', (chapter, date))
-            conn.commit()
-
-    return redirect('/')
+            cursor.execute('SELECT id FROM manga_list WHERE manga_name = ?', (manga_name,))
+            result = cursor.fetchone()
+            if result:
+                manga_id = result[0]
+                for chapter, date in cleaned_data:
+                    cursor.execute('''
+                        INSERT INTO manga_updates(chapter, date, manga_id)
+                        VALUES (?, ?, ?)
+                    ''', (chapter, date, manga_id))
+                conn.commit()
 
 
 def add_manga_to_db(manga_name, url):
@@ -121,42 +122,21 @@ def add_manga_to_db(manga_name, url):
             VALUES (?,?,?)''', (manga_name, url, image_url))
         conn.commit()
 
+@app.route('/remove-manga', methods=['POST'])
+def remove_manga():
+    manga_name = request.form['manga_name']
+    print(f"Removing manga: {manga_name}") 
+    with sqlite3.connect('manga_list.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute(' DELETE FROM manga_list WHERE manga_name = ?', (manga_name,))
+        conn.commit()
+    return redirect('/')
 
-@app.route('/nano', methods=['POST', 'GET'])
-def nano_machine():
-    if request.method == 'POST':
-        r = requests.get("https://kingofshojo.com/manga/nano-machine/")
-
-        #scrape the html and find the div that contains the chapter lists
-        soup = BeautifulSoup(r.content, 'html.parser')
-        target_div = soup.find('div', {'class' : 'eplister', 'id': 'chapterlist'})
-
-        if target_div:
-            # Clean up the text
-            cleaned_text = target_div.text.strip().split('\n')
-            cleaned_text = [line.strip() for line in cleaned_text if line.strip()]
-
-            # Pair chapters and dates
-            cleaned_data = [(cleaned_text[i], cleaned_text[i + 1]) for i in range(0, len(cleaned_text), 2)]
-
-            # Update the database
-            with sqlite3.connect('manga_updates.db') as conn:
-                cursor = conn.cursor()
-                for chapter, date in cleaned_data:
-                    cursor.execute('''
-                        INSERT INTO manga_updates(chapter, date)
-                        VALUES (?, ?)
-                    ''', (chapter, date))
-                conn.commit()
-            return redirect('/nano-machine')
-
-    return redirect('/nano-machine')
-
-
+@app.route('/manga/<manga_name>')
+def manga_detail(manga_name):
+    return nano(manga_name)
 
 if __name__ == "__main__":
-    manga_list_db()
     init_db()
-    add_manga_to_db("Nano-Machine", "https://kingofshojo.com/manga/nano-machine/")
     app.debug(True)
     app.run()
